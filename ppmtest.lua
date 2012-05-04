0692a7d5ae23d061c1598bf28d4a9c9a622d1d77
scythe <scythe@ortsz.com>
2012-05-04 03:20:01 -0400

    minor

--- ppmtest.lua
+++ ppmtest.lua
@@ -1,14 +1,2 @@
 
-function ppmhead(file, width, height)
-  file:write("P6\n" .. width .. "\n" .. height .. "\n255\n")
-end
 
-ppmhead(io.stdout, 100, 100)
-
-for i = 1, 100 do
-  for j = 1, 100 do
-    io.write("\0")
-    io.write(string.char(254))
-    io.write("\0")
-  end
-end
